43007311f3fabae1b6bf42fa5d59dc523883bde4
wangshiliang752 <lw-00@163.com>
2018-04-05 14:58:20 +0800

    123

--- src/main/java/baseinfo/web/SqlSessionFactoryUtils.java
+++ src/main/java/baseinfo/web/SqlSessionFactoryUtils.java
@@ -1,33 +1,62 @@
 package baseinfo.web;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+import baseinfo.dao.iRoleDAO;
+import baseinfo.dto.iRoleDTO;
 
 public class SqlSessionFactoryUtils {
 	private static SqlSessionFactory sqlSessionFactory = null;
 	
 	private final static Class<SqlSessionFactoryUtils> LOCK = SqlSessionFactoryUtils.class;
 
+//	public static SqlSessionFactory getSqlSessionFactory() {
+//		synchronized (LOCK) {
+//			if (sqlSessionFactory != null) {
+//				return sqlSessionFactory;
+//			}
+//			String resource = "mybatis.xml";
+//			InputStream inputStream;
+//			try {
+//				inputStream = Resources.getResourceAsStream(resource);
+//				sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//				return null;
+//			}
+//			return sqlSessionFactory;
+//		}
+//	}
+	
 	public static SqlSessionFactory getSqlSessionFactory() {
 		synchronized (LOCK) {
-			if (sqlSessionFactory != null) {
-				return sqlSessionFactory;
-			}
-			String resource = "mybatis.xml";
-			InputStream inputStream;
-			try {
-				inputStream = Resources.getResourceAsStream(resource);
-				sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return null;
-			}
-			return sqlSessionFactory;
+			//���ݿ����ӳ���Ϣ
+			PooledDataSource dataSource = new PooledDataSource();
+			dataSource.setDriver("com.mysql.jdbc.Driver");
+			dataSource.setUsername("root");
+			dataSource.setPassword("123456");
+			dataSource.setUrl("jdbc:mysql://localhost:3306/chapter3");
+			dataSource.setDefaultAutoCommit(false);
+			//����MyBatis��JDBC����ʽ
+			TransactionFactory transactionFactory = new JdbcTransactionFactory();
+			Environment environment = new Environment("development", transactionFactory, dataSource);
+			//����Configuration����
+			Configuration configuration = new Configuration(environment);
+			//ע��һ��MyBatis�����ı���
+			configuration.getTypeAliasRegistry().registerAlias("role", iRoleDTO.class);
+			//����һ��ӳ����
+			configuration.addMapper(iRoleDAO.class);
+			//ʹ��SqlSessionFactoryBuilder����SqlSessionFactory
+			sqlSessionFactory = 
+			    new SqlSessionFactoryBuilder().build(configuration);
+			return sqlSessionFactory; 	
 		}
 	}
 	
--- src/main/java/baseinfo/web/iRoleController.java
+++ src/main/java/baseinfo/web/iRoleController.java
@@ -13,13 +13,12 @@ public class iRoleController {
 	}
 	
 	private static void testRoleMapper() {
-		Logger log = Logger.getLogger(iRoleController.class);
 		SqlSession sqlSession = null;
 		try {
 			sqlSession = SqlSessionFactoryUtils.openSqlSession();
 			iRoleDAO roleMapper = sqlSession.getMapper(iRoleDAO.class);
 			iRoleDTO role = roleMapper.getRole(1L);
-			log.info(role.getRoleName());
+//			log.info(role.getRoleName());
 		} finally {
 			if (sqlSession != null) {
 				sqlSession.close();
